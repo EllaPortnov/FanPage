61989748de3e6ace509d245465fecaa5143fb3e4
idoperlman <idoperlman2@gmail.com>
2019-04-26 12:19:48 +0300

    add user handling

---
+++ InternetFanPage/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using InternetFanPage.Models;
+using InternetFanPage.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetFanPage.Controllers
+{
+    public class UserController : Controller
+    {
+        UserServices userService = new UserServices();
+
+        public ActionResult Register(RegisterDetails details)
+        {
+            if (userService.Register(details))
+                return View();
+
+            return View(500);
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InternetFanPage/Services/UserServices.cs
@@ -0,0 +1,69 @@
+﻿using InternetFanPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace InternetFanPage.Services
+{
+    public class UserServices
+    {
+        public bool Register(RegisterDetails UserInput)
+        {
+            if (UserInput is null)
+                return false;
+            using (var Context = new PageContext())
+            {
+                User newUser = new User()
+                {
+                    Address = UserInput.Address,
+                    FirstName = UserInput.FirstName,
+                    IsAdmin = 0,
+                    LastName = UserInput.LastName,
+                    Password = convertToMd5(UserInput.Password),
+                    Phone = UserInput.Phone,
+                    Username = UserInput.Username
+                };
+
+                Context.Users.Add(newUser);
+                Context.SaveChanges();
+            }
+            return true;
+        }
+
+        private string convertToMd5(string target)
+        {
+            var cipher = new MD5CryptoServiceProvider();
+            var passwordBytes = Encoding.UTF8.GetBytes(target);
+
+            passwordBytes = cipher.ComputeHash(passwordBytes);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < passwordBytes.Length; i++)
+            {
+                sb.Append(passwordBytes[i].ToString("x2").ToLower());
+            }
+
+            return sb.ToString();
+        }
+
+        public User GetUser(string userName)
+        {
+            using (var context = new PageContext())
+            {
+                return context.Users.FirstOrDefault(u => u.Username == userName);
+            }
+        }
+
+        public bool DoesUserExists(string username)
+        {
+            using (var context = new PageContext())
+            {
+                return context.Users.Count(u => u.Username == username) > 0;
+            }
+        }
+    }
+}
