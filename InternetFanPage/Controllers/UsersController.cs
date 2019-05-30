7375c5d6cfd1e3a7b1ad40e46254e7f7a79e3ca6
Ellaportnov <ellaportnov@gmail.com>
2019-05-30 15:24:53 +0300

    wip

---
+++ InternetFanPage/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using InternetFanPage.Models;
+using InternetFanPage.Services;
+//using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+namespace InternetFanPage.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserServices userService = new UserServices();
+
+        public ActionResult AttemptLogin(LoginDetails details)
+        {
+            LoginResult result = userService.AttemptLogin(details);
+            if (result.LoginSucceeded)
+            {
+                User user = userService.GetUser(details.Username);
+               // HttpContext.Session.Set("User", Encoding.ASCII.GetBytes(user.FirstName));
+               //HttpContext.Session.Set("IsAdmin", BitConverter.GetBytes(user.IsAdmin == 1));
+            }
+            return View();
+        }
+
+
+        public ActionResult Register(RegisterDetails details)
+        {
+            return View();
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- InternetFanPage/Services/UserServices.cs
+++ InternetFanPage/Services/UserServices.cs
@@ -1,11 +1,9 @@
 ﻿using InternetFanPage.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
-using System.Web.Http.Results;
+
 
 namespace InternetFanPage.Services
 {
