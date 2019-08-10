2b4229306f57e4c924c41d58fbc0e0736f2ebc1e
Tom Langer <langer.tom7@gmail.com>
2019-08-10 21:37:14 +0300

    home controller

--- InternetFanPage/Controllers/HomeController.cs
+++ InternetFanPage/Controllers/HomeController.cs
@@ -7,6 +7,102 @@ namespace InternetFanPage.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private FanPageContext ct = new FanPageContext();
+        ShopService shopService = new ShopService();
+        ProductsPageModel model = new ProductsPageModel();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult Concerts()
+        {
+            return View(ct.Concerts.AsEnumerable());
+        }
+
+        public ActionResult Manager()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            //if (Session["UserID"] != null)
+            //{
+            //    int userId = int.Parse(Session["UserID"].ToString());
+
+            //    return View(shopService.RecommendProducts(userId));
+            //}
+
+            return View();
+        }
+
+        //public ActionResult Products()
+        //{
+
+        //    model.Products = shopService.GetAllProductsFromInventory();
+        //    model.Categories = shopService.GetAllCategories();
+
+        //    return View(model);
+        //}
+
+        public ActionResult Products(string searchTerm, int categoryId = -1)
+        {
+            //LoadUserData();
+
+            if (categoryId > 0)
+            {
+                model.Products = shopService.GetProductsByCategory(categoryId);
+            }
+            else
+            {
+                model.Products = shopService.SearchProducts(searchTerm ?? string.Empty);
+            }
+
+            if (model.Categories == null)
+            {
+                model.Categories = shopService.GetAllCategories();
+            }
+
+            return View(model);
+        }
+
+
+        public ActionResult SearchProducts(string term)
+        {
+            return View(shopService.SearchProducts(term));
+        }
+
+        public ActionResult Error()
+        {
+           // return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+           return null;
+        }
+
+        [HttpGet]
+        public ActionResult GetProduct(int id)
+        {
+            var product = shopService.GetProduct(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Json(product, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult BuyProduct(int userId,int id)
+        {
+            return Json(shopService.BuyProduct(userId, id), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            return Json(shopService.UpdateProduct(product));
+        }
     }
 }
